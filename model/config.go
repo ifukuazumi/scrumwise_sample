53b5444d9c8f3d3e18dac2439c2890bf606eb7a8
井福あずみ <ifuku.azumi@gmail.com>
2020-06-23 09:06:55 +0900

    debug

--- model/config.go
+++ model/config.go
@@ -4,6 +4,8 @@ import "github.com/kelseyhightower/envconfig"
 
 // AppConfig プログラム実行時に指定する環境変数の構造体を用意
 type AppConfig struct {
+	//0または指定なし:false , 1を指定:true
+	DebugMode bool `envconfig:"DEBUG_MODE" default:"false"`
 	//APP_ENVに指定がない場合、local
 	AppEnv string `envconfig:"APP_ENV" default:"local"`
 }
